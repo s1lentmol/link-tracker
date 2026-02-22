cea8a030dd585c3bbd28732ea58411a7fa03887d
s1lentmol <artem.molchanov.1337@mail.ru>
2026-02-22 14:51:57 +0300

    Add configuration loading for Telegram bot using viper

---
+++ internal/bot/config/config.go
@@ -0,0 +1,32 @@
+package config
+
+import (
+	"fmt"
+
+	"github.com/spf13/viper"
+)
+
+type Config struct {
+	AppTelegramToken string `mapstructure:"app_telegram_token"`
+}
+
+func Load() (*Config, error) {
+	v := viper.New()
+
+	v.SetConfigFile(".env")
+	_ = v.ReadInConfig()
+
+	v.AutomaticEnv()
+	v.BindEnv("app_telegram_token")
+
+	var cfg Config
+	if err := v.Unmarshal(&cfg); err != nil {
+		return nil, fmt.Errorf("failed to unmarshal config: %w", err)
+	}
+
+	if cfg.AppTelegramToken == "" {
+		return nil, fmt.Errorf("APP_TELEGRAM_TOKEN is required")
+	}
+
+	return &cfg, nil
+}
