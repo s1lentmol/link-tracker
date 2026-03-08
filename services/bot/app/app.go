25c5f1c5bca3f12239eaf5daaaa99f63da421f37
s1lentmol <artem.molchanov.1337@mail.ru>
2026-03-08 17:43:15 +0300

    move logic from main to app and add configuration parameters

---
+++ services/bot/app/app.go
@@ -0,0 +1,99 @@
+package app
+
+import (
+	"errors"
+	"fmt"
+	"log/slog"
+	"net"
+	"os"
+	"os/signal"
+	"syscall"
+
+	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
+	"google.golang.org/grpc"
+
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/pkg/grpcx"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/services/bot/config"
+	grpcadapter "gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/services/bot/internal/adapter/grpc"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/services/bot/internal/adapter/storage"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/services/bot/internal/adapter/telegram"
+	handler "gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/services/bot/internal/controller/telegram"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/services/bot/internal/usecase/user"
+	"gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/shared/pb"
+)
+
+func Run(logger *slog.Logger) error {
+	cfg, err := config.Load()
+	if err != nil {
+		return fmt.Errorf("load config: %w", err)
+	}
+
+	bot, err := telegram.New(cfg.AppTelegramToken)
+	if err != nil {
+		return fmt.Errorf("create telegram bot: %w", err)
+	}
+
+	scrapperClient, err := grpcadapter.NewScrapperClient(cfg.ScrapperGRPCAddr, cfg.GRPCTimeout, logger)
+	if err != nil {
+		return fmt.Errorf("create scrapper grpc client: %w", err)
+	}
+	defer func() {
+		if err := scrapperClient.Close(); err != nil {
+			logger.Warn("failed to close scrapper grpc connection", slog.String("error", err.Error()))
+		}
+	}()
+
+	grpcListener, err := net.Listen("tcp", cfg.BotGRPCAddr)
+	if err != nil {
+		return fmt.Errorf("listen bot grpc on %s: %w", cfg.BotGRPCAddr, err)
+	}
+
+	grpcServer := grpc.NewServer(grpc.UnaryInterceptor(grpcx.UnaryServerLogger(logger)))
+	pb.RegisterBotServiceServer(grpcServer, grpcadapter.NewBotUpdatesServer(bot, logger))
+
+	serveErr := make(chan error, 1)
+	go func() {
+		logger.Info("bot grpc server started", slog.String("addr", cfg.BotGRPCAddr))
+		if err := grpcServer.Serve(grpcListener); err != nil {
+			if !errors.Is(err, grpc.ErrServerStopped) {
+				serveErr <- err
+			}
+		}
+	}()
+
+	logger.Info("bot authorized", slog.String("username", bot.GetUserName()))
+
+	userRepo := storage.NewUserRepository()
+	userUseCase := user.NewUseCase(userRepo)
+	h := handler.New(bot, userUseCase, logger, handler.WithScrapperService(scrapperClient))
+
+	u := tgbotapi.NewUpdate(0)
+	u.Timeout = 60
+	updates := bot.GetUpdatesChan(u)
+
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+
+	logger.Info("bot started, listening for updates...")
+
+	for {
+		select {
+		case update, ok := <-updates:
+			if !ok {
+				logger.Warn("updates channel closed, shutting down bot loop")
+				grpcServer.GracefulStop()
+				return nil
+			}
+			h.HandleUpdate(update)
+		case sig := <-quit:
+			logger.Info("shutting down bot", slog.String("signal", sig.String()))
+			bot.StopReceivingUpdates()
+			grpcServer.GracefulStop()
+			return nil
+		case err := <-serveErr:
+			bot.StopReceivingUpdates()
+			grpcServer.GracefulStop()
+			return fmt.Errorf("bot grpc server stopped unexpectedly: %w", err)
+		}
+	}
+}
--- services/bot/config/config.go
+++ services/bot/config/config.go
@@ -1,30 +1,34 @@
 package config
 
 import (
-	"errors"
 	"fmt"
 	"strings"
+	"time"
 
-	"github.com/spf13/viper"
+	configpkg "gitlab.education.tbank.ru/backend-academy-go-2025/homeworks/link-tracker/pkg/config"
 )
 
 type Config struct {
-	AppTelegramToken string `mapstructure:"app_telegram_token"`
+	AppTelegramToken string        `mapstructure:"app_telegram_token"`
+	BotGRPCAddr      string        `mapstructure:"bot_grpc_addr"`
+	ScrapperGRPCAddr string        `mapstructure:"scrapper_grpc_addr"`
+	GRPCTimeout      time.Duration `mapstructure:"grpc_timeout"`
 }
 
 func Load() (*Config, error) {
-	v := viper.New()
-
-	v.SetConfigFile(".env")
-	if err := v.ReadInConfig(); err != nil {
-		var notFoundErr viper.ConfigFileNotFoundError
-		if !errors.As(err, &notFoundErr) {
-			return nil, fmt.Errorf("failed to read config file: %w", err)
-		}
+	v, err := configpkg.NewViperFromEnvFile(".env")
+	if err != nil {
+		return nil, err
 	}
 
-	v.AutomaticEnv()
-	v.BindEnv("app_telegram_token")
+	_ = v.BindEnv("app_telegram_token")
+	_ = v.BindEnv("bot_grpc_addr")
+	_ = v.BindEnv("scrapper_grpc_addr")
+	_ = v.BindEnv("grpc_timeout")
+
+	v.SetDefault("bot_grpc_addr", ":8082")
+	v.SetDefault("scrapper_grpc_addr", "localhost:8081")
+	v.SetDefault("grpc_timeout", "3s")
 
 	var cfg Config
 	if err := v.Unmarshal(&cfg); err != nil {
@@ -32,8 +36,20 @@ func Load() (*Config, error) {
 	}
 
 	cfg.AppTelegramToken = strings.TrimSpace(cfg.AppTelegramToken)
+	cfg.BotGRPCAddr = strings.TrimSpace(cfg.BotGRPCAddr)
+	cfg.ScrapperGRPCAddr = strings.TrimSpace(cfg.ScrapperGRPCAddr)
+
 	if cfg.AppTelegramToken == "" {
 		return nil, fmt.Errorf("APP_TELEGRAM_TOKEN is required")
+	}
+	if cfg.BotGRPCAddr == "" {
+		return nil, fmt.Errorf("BOT_GRPC_ADDR is required")
+	}
+	if cfg.ScrapperGRPCAddr == "" {
+		return nil, fmt.Errorf("SCRAPPER_GRPC_ADDR is required")
+	}
+	if cfg.GRPCTimeout <= 0 {
+		return nil, fmt.Errorf("GRPC_TIMEOUT must be positive")
 	}
 
 	return &cfg, nil
